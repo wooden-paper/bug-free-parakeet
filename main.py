5784651c097d78175774a69d06d6ef8736be8c60
the-paper-thinker <110599130+the-paper-thinker@users.noreply.github.com>
2022-08-18 12:26:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 from modules.validation import validate_csv
-from modules.dates import retrieve_dates
-from flask import Flask, request
+from modules.dates import *
+from flask import Flask, request, Response
 from modules.ftp import MyFTP
+from creds import creds
 import json
+import os
 
 app = Flask(__name__)
+ftp = MyFTP(**creds)
 
 
 @app.route("/api/get/<file_name>")
-def hello_world(file_name: str):
+def getfile(file_name: str):
     try:
-        with open(f"data/{file_name}", "r") as f:
-            return f
-    except:
-        return json.stringify({"message": "File not found"}), 404
+        f = open(f"{os.getcwd()}/temp/{file_name}", "wb")
+        ftp.download_local(file_name, f)
+        f.close()
+        data = open(f"{os.getcwd()}/temp/{file_name}", "rb").read()
+        os.remove(f"{os.getcwd()}/temp/{file_name}")
+
+        return Response(
+            data,
+            mimetype="text/csv",
+            headers={"Content-disposition": f"attachment; filename={file_name}"},
+        )
+    except Exception as e:
+        return json.stringify({"message": "File not found", "error": e}), 404
 
 
-@app.route("/api/validate/<file_name>")
-def validate(file_name: str):
+@app.route("/api/validate")
+def validate():
     date_from = request.args.get("from", default=get_date(-7), type=str)
     to = request.args.get("to", default=get_date(), type=str)
-    file_names = get_files(date_from, to)
-    validity = []
+    file_names, validity = retrieve_dates(ftp, date_from, to)
+    print(file_names)
     for file in file_names:
-        validity.append(run_validation(file))  # format {"valid":bool, "name": str}
+        f = open(f"{os.getcwd()}/temp/{file}", "wb")
+        ftp.download_local(file, f)
+        f.close()
+        validity.append(
+            validate_csv(file, open(f"{os.getcwd()}/temp/{file}", "r"))
+        )  # format {"valid":bool, "name": str}
+        os.remove(f"{os.getcwd()}/temp/{file}")
     valid = [a for a in validity if a["valid"] == True]
+    return json.dumps(validity)
