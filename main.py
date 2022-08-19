2696bb88bc8dac43169fd7b05b830de7b64666c7
the-paper-thinker <110599130+the-paper-thinker@users.noreply.github.com>
2022-08-19 13:03:28 +0100

    Update main.py
    Add log getting

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ from modules.dates import *
 from flask import Flask, request, Response
 from modules.ftp import MyFTP
 from creds import creds
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
 ftp = MyFTP(**creds)
+CORS(app)
 
 
 @app.route("/api/get/<file_name>")
 def getfile(file_name: str):
+    if file_name == "logs":
+        data = open(f"{os.getcwd()}/logfile.txt", "rb").read()
+        return Response(
+            data,
+            mimetype="text/plain",
+            headers={"Content-disposition": f"attachment; filename={file_name}.txt"},
+        )
     try:
         f = open(f"{os.getcwd()}/temp/{file_name}", "wb")
         try:
             ftp.download_local(file_name, f)
             f.close()
         except:
+            os.remove(f"{os.getcwd()}/temp/{file_name}")
+
             return (
                 Response(
                     json.dumps({"message": "File not found"}),
@@ -35,6 +46,7 @@ def getfile(file_name: str):
             headers={"Content-disposition": f"attachment; filename={file_name}"},
         )
     except:
+
         return (
             Response(
                 json.dumps({"message": "File not found"}), mimetype="application/json"
@@ -45,10 +57,17 @@ def getfile(file_name: str):
 
 @app.route("/api/validate")
 def validate():
-    date_from = request.args.get("from", default=get_date(-7), type=str)
-    to = request.args.get("to", default=get_date(), type=str)
+    try:
+        date_from = request.args.get("from", default=get_date(-7), type=str)
+        to = request.args.get("to", default=get_date(), type=str)
+    except:
+        return (
+            Response(
+                json.dumps({"message": "Invalid format"}), mimetype="application/json"
+            ),
+            403,
+        )
     file_names, validity = retrieve_dates(ftp, date_from, to)
-    print(file_names)
     for file in file_names:
         f = open(f"{os.getcwd()}/temp/{file}", "wb")
         ftp.download_local(file, f)
@@ -57,5 +76,4 @@ def validate():
             validate_csv(file, open(f"{os.getcwd()}/temp/{file}", "r"))
         )  # format {"valid":bool, "name": str}
         os.remove(f"{os.getcwd()}/temp/{file}")
-    valid = [a for a in validity if a["valid"]]
     return Response(json.dumps(validity), mimetype="application/json")
